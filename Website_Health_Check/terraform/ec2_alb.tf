f2e6d7bc2835113c32d7d594c515fd785ceefee7
Rakesh <rksingh2391998@gmail.com>
2026-08-09 04:39:26 +0530

    Website Health Check

--- Website_Health_Check/terraform/ec2_alb.tf
+++ Website_Health_Check/terraform/ec2_alb.tf
@@ -1,30 +1,4 @@
 # =========================================================
-# Ubuntu AMI
-# =========================================================
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  owners = ["0997201094779"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
-
-# =========================================================
 # EC2 INSTANCE
 # =========================================================
 
