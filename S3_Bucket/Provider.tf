fb5d5305976a80188efdc014d8e3c544858cffc3
Rakesh <rksingh2391998@gmail.com>
2026-09-07 03:11:52 +0530

    S3 Bucket pipeline

--- S3_Bucket/Provider.tf
+++ S3_Bucket/Provider.tf
@@ -1,7 +1,16 @@
 # terraform provider
-/*
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # version = "~> 6.0"
+    }
+  }
+
+  # required_version = ">= 1.5.0"
+}
+
 provider "aws" {
   region = "us-east-1"
-  access_key = "12345" # relpace with correct creds
-  secret_key = "12345" # replace with correct creds
-}*/
+}
--- S3_Bucket/s3_bucket.tf
+++ S3_Bucket/s3_bucket.tf
@@ -1,9 +1,21 @@
 # AWS S3 bucket instance
 
-resource aws_s3_bucket my_bucket {
-    bucket = "rak6848"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "rakesh2027"
+
+  tags = {
+    Name        = "rakesh_s3_terraform"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
 }
 
-resource aws_s3_bucket my_s3_bucket {
-    bucket = "rak98922"
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "rakesh98922"
+
+  tags = {
+    Name        = "rakesh_s3_bucket"
+    Environment = "Prod"
+    ManagedBy   = "Terraform"
+  }
 }
